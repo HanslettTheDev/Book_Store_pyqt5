9236a9b4228cae3e7e51ffffd9a0277151e7f10c
HanslettTheDev <HanslettTheDev@gmail.com>
2024-04-14 11:00:47 +0100

    ref(decompiler): improved the decompiler and removed hardcoded values
    Signed-off-by: HanslettTheDev <HanslettTheDev@gmail.com>

--- utility_scripts/decomp.py
+++ utility_scripts/decomp.py
@@ -1,45 +1,53 @@
 from zipfile import ZipFile
-from enigma.machine import EnigmaMachine
 import time
 import os
 
 class DECOMPRESS:
-	def __init__(self):
-		self.zipfile = ZipFile("Cartronic Files.zip")
-
+	def __init__(self, app_name:str, name:str):
+		self.name = name
+		self.app_name = app_name
+		try:
+			self.zipfile = ZipFile(self.name)
+		except FileNotFoundError:
+			print(f"You need the \"{self.name}\" to decompile")
+			input("")
+			import sys
+			sys.exit(1)
 	def start_screen(self):
 		print("==========================")
-		print("Cartronic Decompressor V1.0")
+		print(self.app_name)
 		print("==========================")
 		time.sleep(2)
 		print("")
 		print("Developed By HanslettTheDev")
 		print("Checking Archive for any error")
 		try:
-			ZipFile('Cartronic Files.zip').testzip()
+			ZipFile(self.name).testzip()
 		except Exception as e:
 			print("File may be corrupt or not found", e)
 			input("Enter a key to exit: ")
 		else:
-			ZipFile("Cartronic Files.zip").printdir()
+			ZipFile(self.name).printdir()
 			print("")
 			print("Check Successful")
 
 			print("Please wait until the process is completed")
 			print("Archive will be deleted once completed")
 			try:
-				with ZipFile('Cartronic Files.zip', 'r') as zipobj:
+				with ZipFile(self.name, 'r') as zipobj:
 					filenames = zipobj.namelist()
 					for filename in filenames:
 						print("Decompressing file", filename)
 						zipobj.extract(filename, os.path.join(os.environ['WINDIR'].split(':\\')[0] + ":\\", "ProgramData"))
 					zipobj.close()
 				print("Process Completed")
-				os.remove("Cartronic Files.zip")
+				os.remove(self.name)
 				input("Enter a key to exit: ")
 			except Exception as e:
 				print("Something went wrong here", e)
 				input("Press any Key and hit enter to exit: ")
 
 if __name__ == '__main__':
-	DECOMPRESS().start_screen()
+	app_name = "EcuFixTronic Decompiler V2.0"
+	name = "EcuFixTronic Files.zip"
+	DECOMPRESS(app_name, name).start_screen()
